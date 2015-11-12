dca12fc8e454df38024fef6a26394f206a50c06b
prettyok <jenny@Jennys-MacBook-Pro.local>
2015-11-11 20:10:48 -0600

    Fixed Social Buttons

--- js/animation-scroll.js
+++ js/animation-scroll.js
@@ -3,8 +3,9 @@ $(document).ready(function () {
     $(window).scroll(function() {
 
         var verticalScroll = $(this).scrollTop();
+         console.log(verticalScroll);
 
-        if(verticalScroll >= 2200) {
+        if(verticalScroll >= 2140) {
 
             $('#find-me').addClass('animated slideInLeft');
             $('#find-me').removeClass('hide_me');
